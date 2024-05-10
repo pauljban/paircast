e7edfeec87fcd656b9150537bf231221ae286ed7
Paul Bangalan <160093920+pauljban@users.noreply.github.com>
2024-05-09 23:52:46 -0400

    Complete talwindcss with Vite from docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: ["./index.html", "./src/**/*.{html,js,tsx}"],
   theme: {
     extend: {},
   },
